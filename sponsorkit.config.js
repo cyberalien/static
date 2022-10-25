eb7e31d1f1c6cd719e8eca21f39c5cb61e5be464
Vjacheslav Trushkin <cyberalien@gmail.com>
2022-10-25 12:42:39 +0300

    First commit

---
+++ sponsorkit.config.js
@@ -0,0 +1,35 @@
+import { defineConfig, presets } from 'sponsorkit';
+
+export default defineConfig({
+	// Providers configs
+	github: {
+		login: 'cyberalien',
+		type: 'user',
+	},
+
+	// Rendering configs
+	width: 800,
+	formats: ['json', 'svg', 'png'],
+	outputDir: 'static',
+	tiers: [
+		{
+			title: 'Backers',
+			preset: presets.base,
+		},
+		{
+			title: 'Sponsors',
+			monthlyDollars: 20,
+			preset: presets.medium,
+		},
+		{
+			title: 'Silver Sponsors',
+			monthlyDollars: 50,
+			preset: presets.large,
+		},
+		{
+			title: 'Gold Sponsors',
+			monthlyDollars: 100,
+			preset: presets.xl,
+		},
+	],
+});
